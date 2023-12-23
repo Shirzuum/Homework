119108764423869f0445df5925df6d8ef9e23d4c
Shirzuum <shirazum.monir49@gmail.com>
2023-12-22 22:13:17 -0500

    hw04-01 updated

--- src/hw4JavaVariablesInitialized/AboutMe.java
+++ src/hw4JavaVariablesInitialized/AboutMe.java
@@ -2,22 +2,29 @@ package hw4JavaVariablesInitialized;
 
 public class AboutMe {
 
-	public String myNameString = "monir";
+		public String myNameString = "monir";
 
-//	 a variable is declared
-	public int year;
-//	 a variable is initialized 
-	public byte age = 114;
-	public short leanth = 200;
-	public int mySalary = 123456;
-	public long myBankBalance = 24322678684938l;
-	public float myHight = 166.44f;
-	public double myGrade = 34.87897667;
-	public char gender = 'M';
-	public boolean itPlantHuman = false;
+//		 a variable is declared
+		public int year;
+//		 a variable is initialized 
+		public byte age = 114;
+		public short leanth = 200;
+		public int mySalary = 123456;
+		public long myBankBalance = 24322678684938l;
+		public float myHight = 166.44f;
+		public double myGrade = 34.87897667;
+		public char gender = 'M';
+		public boolean isPlantHuman = false;
 
-	public AboutMe() {
-		System.out.println("This is all about Myself"); 
+		public AboutMe() {
+			System.out.println("This is all about Myself");
+		}
+
+		public void aboutMe() {// Method implemented
+			System.out.println("MY name:" + myNameString + "\tMy age:" + age + "\tMY Leanth:" + leanth + "\tMY Salary is:"
+					+ mySalary + "\tbank Balance is:" + myBankBalance + "\tMy hights:" + myHight + "\tMy grade:" + myGrade
+					+ "\tI am:" + gender + "\tIt's:" + isPlantHuman);
+
+		}
+
 	}
-
-}
--- src/hw4JavaVariablesInitialized/AboutMeTest.java
+++ src/hw4JavaVariablesInitialized/AboutMeTest.java
@@ -3,20 +3,7 @@ package hw4JavaVariablesInitialized;
 public class AboutMeTest {
 	public static void main(String[] args) {
 		AboutMe aboutMe = new AboutMe();
-		System.out.println(aboutMe.myNameString); 
-		System.out.println(aboutMe.age);
-		System.out.println(aboutMe.leanth);
-		System.out.println(aboutMe.mySalary);
-		System.out.println(aboutMe.myBankBalance);
-		System.out.println(aboutMe.myHight);
-		System.out.println(aboutMe.myGrade);
-		System.out.println(aboutMe.gender);
-		System.out.println(aboutMe.itPlantHuman);
-
-		System.out.println("MY name:" + aboutMe.myNameString + "\tMy age:" + aboutMe.age + "\tMY Leanth:"
-				+ aboutMe.leanth + "\tMY Salary is:" + aboutMe.mySalary + "\tbank Balance is:" + aboutMe.myBankBalance
-				+ "\tMy hights:" + aboutMe.myHight + "\tMy grade:" + aboutMe.myGrade + "\tI am:" + aboutMe.gender
-				+ "\tIt's:" + aboutMe.itPlantHuman);
+		aboutMe.aboutMe();// Method initialized
 
 	}
 
