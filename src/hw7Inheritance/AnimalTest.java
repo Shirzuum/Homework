6ed7adbf6c3c68274b902e59055bb83b7077fa6b
Shirzuum <shirazum.monir49@gmail.com>
2024-01-08 21:45:00 -0500

    hw

---
+++ src/hw7Inheritance/AnimalTest.java
@@ -0,0 +1,49 @@
+package hw7Inheritance;
+
+public class AnimalTest {
+	public static void main(String[] args) {
+		Animal animal = new Animal();
+		animal.animalInfo();
+//		single inheritance birds
+		Birds birds= new Birds();
+		birds.birdsInfo();
+		
+		
+//		multilevel inheritance
+		
+		BullDog bullDog =new BullDog();
+		bullDog.bulldogInfo();
+		bullDog.doginfo();
+		bullDog.animalInfo();
+		bullDog.mammalInfo();
+		
+		
+		
+		
+//		Hierarchical inheritance
+		Cobra Cobra =new Cobra();
+		Cobra.cobraInfo();
+		Cobra.snakeInfo();
+		Cobra.reptileInfo();
+		Cobra.animalInfo();
+		
+		
+		
+		Dog dog = new Dog();
+		dog.doginfo();
+		Mammal mammal =new Mammal();
+		mammal.mammalInfo();
+		Reptile reptile = new Reptile();
+		reptile.reptileInfo();
+		
+		
+		Robin robin =new Robin();
+		robin.robinInfo();
+		robin.birdsInfo();
+		robin.animalInfo();
+		
+		
+		Snake snake =new Snake();
+		snake.snakeInfo();
+	}
+	}
---
+++ src/hw7Inheritance/BullDog.java
@@ -0,0 +1,8 @@
+package hw7Inheritance;
+
+public class BullDog extends Dog  {
+	public void bulldogInfo() {
+		System.out.println("This method is from BullDog Class");
+	}
+
+}
